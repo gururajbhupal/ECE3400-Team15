43a001b4247c290add2a68e42ed938f42faaabef
Rami Pellumbi <rp534@cornell.edu>
2018-11-15 15:09:55 -0500

    Fixed old code
    Added info file and fixed lab 1 code with changes we made along the way for added consistency

--- Lab1/Code/follow_line/follow_line.ino
+++ Lab1/Code/follow_line/follow_line.ino
@@ -2,13 +2,64 @@
 
 Servo servo_left;
 Servo servo_right;
+
 int sensor_left = A3;
 int sensor_middle = A4;
 int sensor_right = A5;
 
-int left_threshold = 100;
-int middle_threshold = 100;
-int right_threshold = 100;
+int line_thresh = 400; //if BELOW this we detect a white line
+
+/*Initializes the servo*/
+void servo_setup() {
+  servo_right.attach(5);
+  servo_left.attach(6);
+  servo_left.write(90);
+  servo_right.write(90);
+}
+
+
+/*Go Straight*/
+void go() {
+  servo_left.write(100);
+  servo_right.write(80);
+}
+
+
+/*Stop*/
+void halt() {
+  servo_left.write(90);
+  servo_right.write(90);
+}
+
+
+/*Simple left turn*/
+void turn_left() {
+  servo_left.write(93);
+  servo_right.write(70);
+}
+
+
+/*Simple right turn*/
+void turn_right() {
+  servo_left.write(110);
+  servo_right.write(87);
+}
+
+/*Follows the line if a line sensor is on one. Halts movement if all three sensors are not on a line*/
+void linefollow() {
+  if (analogRead(sensor_middle) < line_thresh) {
+    go();
+  }
+  if (analogRead(sensor_left) < line_thresh) {
+    turn_left();
+  }
+  if (analogRead(sensor_right) < line_thresh) {
+    turn_right();
+  }
+  if (analogRead(sensor_right) > line_thresh && analogRead(sensor_left) > line_thresh && analogRead(sensor_middle) > line_thresh) {
+    halt();
+  }
+}
 
 void setup() {
   servo_setup();
@@ -16,50 +67,5 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(analogRead(sensor_left));
-  Serial.println(analogRead(sensor_middle));
-  Serial.println(analogRead(sensor_right));
-  Serial.println();
-  if (analogRead(sensor_middle) < middle_threshold) {
-    go();
-  }
-  if (analogRead(sensor_right) < right_threshold && analogRead(sensor_left) < left_threshold && analogRead(sensor_middle) < middle_threshold) {
-    go();
-  }
-  else if (analogRead(sensor_left) < left_threshold) {
-    turn_left();
-  }
-  else if (analogRead(sensor_right) < right_threshold) {
-    turn_right();
-  }
-  if (analogRead(sensor_right) > right_threshold && analogRead(sensor_left) > left_threshold && analogRead(sensor_middle) > middle_threshold) {
-    halt();
-  }
-}
-
-void servo_setup() {
-  servo_right.attach(10);
-  servo_left.attach(9);
-  servo_left.write(90);
-  servo_right.write(90);
-}
-
-void go() {
-  servo_left.write(95);
-  servo_right.write(85);
-}
-
-void halt() {
-  servo_left.write(90);
-  servo_right.write(90);
-}
-
-void turn_left() {
-  servo_left.write(90);
-  servo_right.write(85);
-}
-
-void turn_right() {
-  servo_left.write(95);
-  servo_right.write(90);
+  linefollow();
 }
